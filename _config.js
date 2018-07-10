dfdae6fef188e1554e1cc4c3532a88c43221faba
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-07-10 13:59:07 +1000

    added routing and db

--- _config.js
+++ _config.js
@@ -1,8 +1,8 @@
 var config = {};
 
 config.mongoURI = {
-  development: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds125871.mlab.com:25871/cs-collab`,
-  test: `mongodb://${process.env.DB_USER_TEST}:${process.env.DB_PASS_TEST}@ds127841.mlab.com:27841/ca-collab-test`
+  development: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds123490.mlab.com:23490/mikew-production`,
+  test: `mongodb://${process.env.DB_USER_TEST}:${process.env.DB_PASS_TEST}@ds233061.mlab.com:33061/mikew-test`
 };
 
 module.exports = config;
